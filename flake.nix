076c7bf6077bf9d97508ca6c728c9c6068664f02
Frost-Phoenix <67cyril6767@gmail.com>
2024-07-31 11:14:39 +0200

    build my own package from github

--- flake.nix
+++ flake.nix
@@ -4,20 +4,20 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nur.url = "github:nix-community/NUR";
-  
+
     hypr-contrib.url = "github:hyprwm/contrib";
     hyprpicker.url = "github:hyprwm/hyprpicker";
-  
+
     alejandra.url = "github:kamadorueda/alejandra/3.0.0";
-  
+
     nix-gaming.url = "github:fufexan/nix-gaming";
-  
+
     hyprland = {
       type = "git";
       url = "https://github.com/hyprwm/Hyprland";
       submodules = true;
     };
-  
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -40,35 +40,35 @@
     spicetify-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, self, ...} @ inputs:
-  let
-    selfPkgs = import ./pkgs;
-    username = "john";
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
+  outputs = { nixpkgs, self, ... } @ inputs:
+    let
+      selfPkgs = import ./pkgs;
+      username = "john";
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      lib = nixpkgs.lib;
+    in
+    {
+      overlays.default = selfPkgs.overlay;
+      nixosConfigurations = {
+        desktop = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [ (import ./hosts/desktop) ];
+          specialArgs = { host = "desktop"; inherit self inputs username; };
+        };
+        laptop = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [ (import ./hosts/laptop) ];
+          specialArgs = { host = "laptop"; inherit self inputs username; };
+        };
+        vm = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [ (import ./hosts/vm) ];
+          specialArgs = { host = "vm"; inherit self inputs username; };
+        };
+      };
     };
-    lib = nixpkgs.lib;
-  in
-  {
-    overlays.default = selfPkgs.overlay;
-    nixosConfigurations = {
-      desktop = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [ (import ./hosts/desktop) ];
-        specialArgs = { host="desktop"; inherit self inputs username ; };
-      };
-      laptop = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [ (import ./hosts/laptop) ];
-        specialArgs = { host="laptop"; inherit self inputs username ; };
-      };
-       vm = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [ (import ./hosts/vm) ];
-        specialArgs = { host="vm"; inherit self inputs username ; };
-      };
-    };
-  };
 }
