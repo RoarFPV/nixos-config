d7ba33bf9bd4054bdb5b5a321e7f168ea861fc0d
RoarFPV <roarfpv@gmail.com>
2024-08-11 09:19:48 -0700

    Updated packages

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -1,33 +1,33 @@
-{ inputs, pkgs, ... }: 
+{ inputs, pkgs, ... }:
 {
   home.packages = (with pkgs; [
     audacity
-    bitwise                           # cli tool for bit / hex manipulation
-    evince                            # gnome pdf viewer
-    eza                               # ls replacement
-    entr                              # perform action when file change
-    fd                                # find replacement
-    file                              # Show file information 
-    fzf                               # fuzzy finder
-    gtt                               # google translate TUI
-    gifsicle                          # gif utility
+    bitwise # cli tool for bit / hex manipulation
+    evince # gnome pdf viewer
+    eza # ls replacement
+    entr # perform action when file change
+    fd # find replacement
+    file # Show file information 
+    fzf # fuzzy finder
+    gtt # google translate TUI
+    gifsicle # gif utility
     gimp
-    gtrash                            # rm replacement, put deleted files in system trash
+    gtrash # rm replacement, put deleted files in system trash
     hexdump
-    jdk17                             # java
+    jdk17 # java
     lazygit
     libreoffice
-    cinnamon.nemo-with-extensions     # file manager
-    nitch                             # systhem fetch util
+    cinnamon.nemo-with-extensions # file manager
+    nitch # systhem fetch util
     nix-prefetch-github
-    prismlauncher                     # minecraft launcher
-    ripgrep                           # grep replacement
-    soundwireserver                   # pass audio to android phone
-    tdf                               # cli pdf viewer
-    todo                              # cli todo list
-    toipe                             # typing test in the terminal
-    valgrind                          # c memory analyzer
-    yazi                              # terminal file manager
+    prismlauncher # minecraft launcher
+    ripgrep # grep replacement
+    soundwireserver # pass audio to android phone
+    tdf # cli pdf viewer
+    todo # cli todo list
+    toipe # typing test in the terminal
+    valgrind # c memory analyzer
+    yazi # terminal file manager
     yt-dlp-light
     zenity
     winetricks
@@ -40,24 +40,24 @@
     # Python
     python3
 
-    bleachbit                         # cache cleaner
+    bleachbit # cache cleaner
     cmatrix
-    gparted                           # partition manager
+    gparted # partition manager
     ffmpeg
-    imv                               # image viewer
+    imv # image viewer
     killall
     libnotify
-	  man-pages					            	  # extra man pages
-    mpv                               # video player
-    ncdu                              # disk space
+    man-pages # extra man pages
+    mpv # video player
+    ncdu # disk space
     openssl
-    pamixer                           # pulseaudio command line mixer
-    pavucontrol                       # pulseaudio volume controle (GUI)
-    playerctl                         # controller for media players
-    wl-clipboard                      # clipboard utils for wayland (wl-copy, wl-paste)
-    cliphist                          # clipboard manager
+    pamixer # pulseaudio command line mixer
+    pavucontrol # pulseaudio volume controle (GUI)
+    playerctl # controller for media players
+    wl-clipboard # clipboard utils for wayland (wl-copy, wl-paste)
+    cliphist # clipboard manager
     poweralertd
-    qalculate-gtk                     # calculator
+    qalculate-gtk # calculator
     unzip
     wget
     xdg-utils
@@ -72,5 +72,6 @@
     # spotify
     autoPatchelfHook
     ghidra
+    imhex
   ]);
 }
